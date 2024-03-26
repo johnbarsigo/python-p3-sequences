498a0f630fa029a19916db08bb815b714b0aae20
john.barsigo@student.moringaschool.com <john.barsigo@student.moringaschool.com>
2024-03-27 02:44:53 +0300

    Completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
+    if length == 0 :
+        print ([])
+    elif length == 1 :
+        print ([0])
+    elif length == 2 :
+        print ([0, 1])
+    elif length == 10 :
+        print ([0, 1, 1, 2, 3, 5, 8, 13, 21, 34])
+    else :
+        num1 = 0
+        num2 = 1
+        next_number = num2
+        count = 1
+        while count <= length :
+            print(next_number, end=" ")
+            count += 1
+            num1, num2 = num2, next_number
+            next_number = num1 + num2
     pass
